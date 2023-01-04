859bdd2bb6562a8e40697f51b07bf7d5e174c9cc
Kiven Alaric <elmozaro3@gmail.com>
2023-01-04 05:07:51 -0600

    edits of html and css files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './styles/styles.css';
 
-// const plusBtn = document.querySelector('.plus');
-// const minusBtn = document.querySelector('.minus');
-// const textContainer = document.querySelector('span');
+const plusBtn = document.querySelector('.plus');
+const minusBtn = document.querySelector('.minus');
+const textContainer = document.querySelector('.');
 
 // let data = 0;
 
